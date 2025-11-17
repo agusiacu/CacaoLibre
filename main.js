dc1590528057f4477cc1d50e71c2f500ce136577
Iacuzzi, Agustín <164666743+agusiacu@users.noreply.github.com>
2025-11-17 19:36:24 -0300

    index y primer publicación

---
+++ main.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const menuBtn = document.getElementById('menu-btn');
+    const mobileMenu = document.getElementById('mobile-menu');
+    if (menuBtn && mobileMenu) {
+        
+        menuBtn.addEventListener('click', () => {
+            
+            mobileMenu.classList.toggle('menu-open');
+        });
+    }
+
+    let slideIndex = 0;
+    showSlides();
+
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        
+        slideIndex++;
+        
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        
+        if (slides[slideIndex - 1]) {
+            slides[slideIndex - 1].style.display = "block";
+        }
+        
+        setTimeout(showSlides, 4000); 
+    }
+
+});
